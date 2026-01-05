2099586dfac8f690295505cbaa59723f7fdb47ad
Muhammed Atef <ma6919550@gmail.com>
2026-01-05 12:02:22 +0400

    first simple c# progrm

--- muhammed/Program.cs
+++ muhammed/Program.cs
@@ -1,19 +1,10 @@
-﻿using System.Data;
-string day,month,year;
-Console.WriteLine("Enter Date : ");
-DateOnly mydate = DateOnly.Parse(Console.ReadLine());
-Console.WriteLine(mydate.DayOfWeek);
-Console.WriteLine($"{mydate.DayOfWeek} of monthe {mydate:MMMM} yaer {mydate.Year}");
-Console.WriteLine("what is your name ?");
-string name = Console.ReadLine();
-Console.WriteLine($"name is {name}");
-
-Console.WriteLine("what is your last job ?");
-string job = Console.ReadLine();
-Console.WriteLine($"his last job is {job}");
-Console.WriteLine("what is your current and desired salary ?");
-decimal lastSalary, SalaryNow;
-lastSalary=decimal.Parse(Console.ReadLine());
-SalaryNow = decimal.Parse(Console.ReadLine());
-Console.WriteLine($"current salary : {lastSalary} , Desired Salary : {SalaryNow}");
+﻿int x = 4 , y = 6 ;
+if (x < y)
+    Console.WriteLine($" No y is {y}" + $" it's begger than x = {x}");
+else
+    Console.WriteLine("Y is less than X");
+var result = x < y ? "x is less than y " : "x is bedder than y";
+Console.WriteLine(result);
+var evenOrodd = (y % 2 == 0) ? " even " : "odd";
+Console.WriteLine(evenOrodd);
 Console.ReadLine();
